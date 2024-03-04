d3ca2488211342e4c3c9a60687623357fd865480
Manjari Srivastav <Manjari.Srivastav@nashtechglobal.com>
2024-03-04 11:17:19 +0530

    Add files via upload

---
+++ .net assessment/Problem5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating activities for a workflow
+            var activities = new List<IActivity>
+            {
+                new EmailActivity(),
+                new FileTransferActivity()
+                // Add more activities as needed
+            };
+
+            // Running the workflow
+            var engine = new WorkflowEngine();
+            engine.Run(activities);
+
+            Console.WriteLine("Workflow completed.");
+        }
+    }
+
+    public interface IActivity
+    {
+        void Execute();
+    }
+}
